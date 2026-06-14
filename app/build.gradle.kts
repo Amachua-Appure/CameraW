0386cf2eccf7fa8de0376e09667f8e38fa408b0a
Amachua-Appure <aishamaidofgreyrats@gmail.com>
2026-06-14 17:33:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "2.4"
+        versionName = "2.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
